3c4eb4ad2eb889c5c24ced9da88a30e27158374e
Amir-NM <20anm00@gmail.com>
2023-06-02 09:10:49 +0330

    backend app completed.

---
+++ src/main/java/api/AlertsController.java
@@ -0,0 +1,23 @@
+package api;
+
+import api.database.Alert;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class AlertsController {
+    private final AlertsService alertsService;
+
+    public AlertsController(AlertsService alertsService) {
+        this.alertsService = alertsService;
+    }
+
+    @GetMapping("/alerts")
+    public ResponseEntity<List<Alert>> getAllAlerts() {
+        return new ResponseEntity<>(this.alertsService.getAllAlerts(), HttpStatus.OK);
+    }
+}
---
+++ src/main/java/api/AlertsService.java
@@ -0,0 +1,25 @@
+package api;
+
+import api.database.Alert;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+@Service
+public class AlertsService {
+    private final AlertRepository alertRepository;
+
+    public AlertsService(AlertRepository alertRepository) {
+        this.alertRepository = alertRepository;
+    }
+
+    public List<Alert> getAllAlerts() {
+        Iterable<Alert> alerts = this.alertRepository.findAll();
+        List<Alert> alertList = new ArrayList<>();
+        alerts.forEach(alertList::add);
+        Collections.sort(alertList);
+        return alertList;
+    }
+}
